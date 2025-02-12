1361de28c1f6c4216703098ab4cca567a2536105
Majdi Aribi <majdi.developer@gmail.com>
2025-02-12 16:52:26 +0100

    add packages and add timer repository

---
+++ lib/features/timer/domain/repositories/timer_repository.dart
@@ -0,0 +1,18 @@
+import 'package:box_round_timer/core/error/failure.dart';
+import 'package:box_round_timer/features/timer/domain/entities/timer_entity.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class TimerRepository {
+  /// Adds a new timer configuration.
+  Future<Either<Failure, Unit>> addTimer(TimerEntity timer);
+
+  /// Updates an existing timer configuration.
+  Future<Either<Failure, Unit>> updateTimer(
+      String id, TimerEntity updatedTimer);
+
+  /// Removes a timer by its unique ID.
+  Future<Either<Failure, Unit>> removeTimer(String id);
+
+  /// Retrieves all saved timers.
+  Future<Either<Failure, List<TimerEntity>>> getAllTimers();
+}
